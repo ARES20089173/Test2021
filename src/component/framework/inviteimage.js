7e8a11f797c68ef1846cce7c2802f401f45cfd76
ARES20089173 <B62951467@gmail.com>
2021-09-23 22:22:40 +0800

    change

---
+++ src/component/framework/inviteimage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import pic1 from './img/testPic1.png'
+export default function inviteimages(){
+    return(
+    <a href="#"><img src={pic1} width="100%" height="120vh"/></a>
+    );
+}
