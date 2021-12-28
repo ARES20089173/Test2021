d04654aef941092f6710f8f05d2774cf90e82845
ARES20089173 <B62951467@gmail.com>
2021-12-28 18:02:34 +0800

    change popup decare height

--- src/component/Pages/Shop/ShopCartSlider.js
+++ src/component/Pages/Shop/ShopCartSlider.js
@@ -38,12 +38,12 @@ const Tooltip2 = () => (
         closeOnDocumentClick={false}
     >
         {close => (
-            <div className="modal">
+            <div className="modal" >
                 <button className="close" onClick={close}>
                 </button>
-                <Grid xs={12} style={{overflow:'scroll'}} height='48vh' >
+                <Grid xs={12} style={{overflow:'scroll'}} height='42vh' >
                     <Typography variant='h6' textAlign='center' color='white'>Puzzle earning on cash spending</Typography>
-                    <Typography variant='caption' textAlign='center' color='white'>
+                    <Typography variant='caption' textAlign='center' color='white' >
                         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
                         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita k
                     </Typography>
